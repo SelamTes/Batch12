b1e023d6b773524fabd29201740d7c96485e5ed3
SELAM <SELAM@192.168.0.7>
2024-09-10 20:36:42 -0400

    add new method

--- src/HomeWork/gitHomework.java
+++ src/HomeWork/gitHomework.java
@@ -3,7 +3,10 @@ package HomeWork;
 public class gitHomework {
 	
 	public static void main (String[] args) {
+		System.out.println("BATCH 12:Git class Lecture");
 		
 	}
-
+public static void add(int a, int b) {
+	System.out.println(a+b);
+}
 }
