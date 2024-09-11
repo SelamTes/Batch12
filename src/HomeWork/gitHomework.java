9ba42eb80fb5d171f8b6791a011b6f0de436874a
SELAM <SELAM@192.168.0.7>
2024-09-10 21:17:32 -0400

    add

--- src/HomeWork/gitHomework.java
+++ src/HomeWork/gitHomework.java
@@ -2,22 +2,4 @@ package HomeWork;
 
 public class gitHomework {
 	
-	public static void main (String[] args) {
-		System.out.println("BATCH 12:Git class Lecture");
-		int age = 12;
-		String name ="John";
-		//this is coming from github
-		
-	}
-public static void add(int a, int b) {
-	System.out.println(a+b);
-}
-
-public static void subtract(int a , int b) {
-	System.out.println(a-b);
-}
-public static void multiply (int a, int b) {
-	
-	System.out.println(a*b);
-}
 }
