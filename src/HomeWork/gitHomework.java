1d023ef98a68eedd3e26331355bd72cf1dfaf712
SELAM <SELAM@192.168.0.7>
2024-09-10 22:07:15 -0400

    Remove all methods

--- src/HomeWork/gitHomework.java
+++ src/HomeWork/gitHomework.java
@@ -1,5 +1,13 @@
 package HomeWork;
 
 public class gitHomework {
-	
-}
+	public void display() {
+		System.out.println("Selam");
+		String firstName = "John";
+		String lastName ="cena";
+		String city = "Reston";
+		
+		}
+	}
+
+
